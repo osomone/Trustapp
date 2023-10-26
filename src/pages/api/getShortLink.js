ac363a7a403eac32abc939955e2f4d63edd2750f
osomone <142011395+osomone@users.noreply.github.com>
2023-10-26 17:34:23 +0800

    Update getShortLink.js

--- src/pages/api/getShortLink.js
+++ src/pages/api/getShortLink.js
@@ -3,11 +3,10 @@ export default async function handler(req, res) {
     const { link } = req.body;
 
     try {
-        // Note the updated endpoint URL and the removed slug as a query param
         const shortenedResponse = await fetch('https://api.dub.co/api/projects/Trustjoy/links', {
             method: 'POST',
             headers: {
-                'Authorization': 'Bearer ABjZKN0UQNRZtLz6k1wJSSun',  // Updated Bearer token
+                'Authorization': 'Bearer ABjZKN0UQNRZtLz6k1wJSSun',
                 'Content-Type': 'application/json'
             },
             body: JSON.stringify({ domain: 'trustjoy.app', url: link })
@@ -17,7 +16,9 @@ export default async function handler(req, res) {
         if (shortenedData.error) {
             throw new Error(shortenedData.error);
         }
-        res.json({ url: shortenedData.domain + '/' + shortenedData.key });
+        
+        // Modify the response to format the URL as desired
+        res.json({ url: 'trustjoy.app/' + shortenedData.key });
     } catch (error) {
         res.status(500).json({ error: error.message || 'Server error' });
     }
