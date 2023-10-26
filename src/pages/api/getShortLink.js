b0906fbb4dfb3f47467d4b256ddc811ba8b6ec8d
osomone <142011395+osomone@users.noreply.github.com>
2023-10-26 17:16:37 +0800

    Update getShortLink.js

--- src/pages/api/getShortLink.js
+++ src/pages/api/getShortLink.js
@@ -3,11 +3,25 @@ export default async function handler(req, res) {
     const { link } = req.body;
 
     try {
-        // Note the updated endpoint URL and the removed slug as a query param
+        const infoResponse = await fetch(`https://api.dub.co/api/projects/Trustjoy/links/info?domain=trustjoy.app&url=${encodeURIComponent(link)}`, {
+            headers: {
+                'Authorization': 'Bearer ABjZKN0UQNRZtLz6k1wJSSun'
+            }
+        });
+
+        const infoData = await infoResponse.json();
+
+        // If the link already exists
+        if (infoData && infoData.id) {
+            res.json({ url: infoData.domain + '/' + infoData.key });
+            return;
+        }
+
+        // If the link doesn't exist, create a new short link
         const shortenedResponse = await fetch('https://api.dub.co/api/projects/Trustjoy/links', {
             method: 'POST',
             headers: {
-                'Authorization': 'Bearer ABjZKN0UQNRZtLz6k1wJSSun',  // Updated Bearer token
+                'Authorization': 'Bearer ABjZKN0UQNRZtLz6k1wJSSun',
                 'Content-Type': 'application/json'
             },
             body: JSON.stringify({ domain: 'trustjoy.app', url: link })
