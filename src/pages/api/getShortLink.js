ee672f66539bfcea3996b67f40624edd88007567
osomone <142011395+osomone@users.noreply.github.com>
2023-10-26 17:11:38 +0800

    Update getShortLink.js

--- src/pages/api/getShortLink.js
+++ src/pages/api/getShortLink.js
@@ -1,15 +1,24 @@
 export default async function handler(req, res) {
-  if (req.method !== 'POST') { return res.status(405).end(); }
-  const { link } = req.body;
-  const shortenedResponse = await fetch('https://api.dub.co/links?slug=Trustjoy', {
-    method: 'POST',
-    headers: {
-      'Authorization': 'Bearer 42ay4mZJ6YbrZbSZlgqs4iDe',
-      'Content-Type': 'application/json'
-    },
-    body: JSON.stringify({ domain: 'trustjoy.app', url: link })
-  });
+    if (req.method !== 'POST') { return res.status(405).end(); }
+    const { link } = req.body;
 
-  const shortenedData = await shortenedResponse.json();
-  res.json({ url: shortenedData.domain + '/' + shortenedData.key});
+    try {
+        // Note the updated endpoint URL and the removed slug as a query param
+        const shortenedResponse = await fetch('https://api.dub.co/api/projects/Trustjoy/links', {
+            method: 'POST',
+            headers: {
+                'Authorization': 'Bearer ABjZKN0UQNRZtLz6k1wJSSun',  // Updated Bearer token
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({ domain: 'trustjoy.app', url: link })
+        });
+
+        const shortenedData = await shortenedResponse.json();
+        if (shortenedData.error) {
+            throw new Error(shortenedData.error);
+        }
+        res.json({ url: shortenedData.domain + '/' + shortenedData.key });
+    } catch (error) {
+        res.status(500).json({ error: error.message || 'Server error' });
+    }
 }
